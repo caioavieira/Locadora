87101f96b3641bedc17ac061b98f865ffb2a933e
Caio Vieira <a.vieiracaio@gmail.com>
2021-07-30 01:55:40 -0300

    Correções estrutura do projeto, docker container visual studio code

--- src/Locadora.WebApp/Controllers/HomeController.cs
+++ src/Locadora.WebApp/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using Locadora.WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Locadora.WebApp.Models;
 
 namespace Locadora.WebApp.Controllers
 {
