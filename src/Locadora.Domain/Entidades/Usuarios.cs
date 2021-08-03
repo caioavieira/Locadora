28d807e23359724de272026282c525d22b7a51b0
jessicaemg <jessicaestevesmg@gmail.com>
2021-08-02 23:22:39 -0300

    Criação da pasta entidades com a classe Usuarios.cs

---
+++ src/Locadora.Domain/Entidades/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Usuarios
+{
+	
+	
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Documento{ get; set; }
+    public int Tipo { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public int Telefone { get; set; }
+    public int DDD { get; set; }
+    public string Endereco { get; set; }
+
+}
