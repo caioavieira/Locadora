2bc51df55f5326dd3641d67edad04ff63537ca9c
Luiz Fernando <luizfernandoo1515@gmail.com>
2021-08-02 23:54:01 -0300

    Entidade Produto

---
+++ src/Locadora.Domain/Entidades/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Documento { get; set; }
+    public int Tipo { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public int Telefone { get; set; }
+    public int DDD { get; set; }
+    public Endereco Endereco { get; set; }
+    public Usuario(int id, string nome, string documento, int tipo, string email, string senha, int telefone, int ddd, Endereco endereco)
+    {
+        Id = id;
+        Nome = nome;
+        Documento = documento;
+        Tipo = tipo;
+        Email = email;
+        Senha = senha;
+        Telefone = telefone;
+        DDD = ddd;
+        Endereco = endereco;
+    }
+
+}
